b515874b2780f0522ab10a4938dc64bdac6e09af
kudys14-dev <kudys14@gmail.com>
2026-03-03 19:37:24 +0100

    Delete 004a_Wedzarnia_2xds18b20_NTC_web_pass_FLASH_tesst directory

--- 004a_Wedzarnia_2xds18b20_NTC_web_pass_FLASH_tesst/tasks.h
+++
@@ -1,9 +0,0 @@
-// tasks.h - Zmodernizowana wersja
-#pragma once
-#include <Arduino.h>
-
-// Tworzenie wszystkich zadań
-void tasks_create_all();
-
-// Status watchdog
-String getTaskWatchdogStatus();
